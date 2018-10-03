7bd99aeebf32e848ba64ecdd6dbc76349f99f655
anirudh-jwala <anirudhsai2@gmail.com>
2018-10-03 21:28:03 +0530

    Final Commit

--- app/src/main/java/in/paperwrk/challengeone/CustomData.java
+++ app/src/main/java/in/paperwrk/challengeone/CustomData.java
@@ -23,13 +23,13 @@ class CustomData {
             "American Express"};
 
     static String[] urls = {
-            "http://diylogodesigns.com/blog/wp-content/uploads/2016/04/google-logo-icon-PNG-Transparent-Background.png",
+            "https://cdn.dribbble.com/users/904380/screenshots/2233565/revised-google-logo.gif",
             "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/2000px-Apple_logo_black.svg.png",
             "http://www.pngmart.com/files/4/Microsoft-Logo-PNG-Photos.png",
             "https://png.icons8.com/color/1600/tesla-logo.png",
             "https://png.icons8.com/windows/1600/amazon.png",
             "http://www.stickpng.com/assets/images/58429073a6515b1e0ad75abc.png",
-            "https://upload.wikimedia.org/wikipedia/commons/3/31/AT%26T_logo_2016.svg",
+            "https://www.att.com/shopcms/media/att/2014/global/social-logo/logo-att-color-trans-200x200.png",
             "https://vignette.wikia.nocookie.net/logopedia/images/b/bd/Ford_logo.png/revision/latest/scale-to-width-down/640?cb=20100623190730",
             "http://pngimg.com/uploads/mercedes_logos/mercedes_logos_PNG11.png",
             "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Reliance_Communications_Logo.svg/1049px-Reliance_Communications_Logo.svg.png",
